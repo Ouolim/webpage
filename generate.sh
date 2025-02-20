07d210ac7c53a231b68995ebef9a51f1abf1e375
ouolim <ouolim@eideo.cz>
2025-02-20 12:15:49 +0100

    Cleanup, moved index.html to root folder

--- generate.sh
+++ generate.sh
@@ -4,6 +4,7 @@ mkdir -p docs/content
 mkdir -p docs/assets
 mkdir -p docs/static
 
+cp index.html docs/
 cp -r content/* docs/content/
 cp -r assets/* docs/assets/
 cp -r static/* docs/static/
@@ -12,6 +13,4 @@ for file in notes/*.md; do
     filename=$(basename "$file" .md)
     pandoc "$file" -s --template=templates/template.html -o "docs/content/$filename.html" --mathjax --metadata title="$file"
 done
-
-
 echo "Local generation complete!"
